e525decb216add4e1cbc4848a3154fac243122a1
Keith <limbokeith71@gmail.com>
2021-07-14 13:38:45 +0800

    disable menu at splachscreen

--- src/app/pages/loader/loader.page.ts
+++ src/app/pages/loader/loader.page.ts
@@ -1,5 +1,6 @@
 import { Component, getModuleFactory, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-loader',
@@ -8,11 +9,21 @@ import { Router } from '@angular/router';
 })
 export class LoaderPage implements OnInit {
 
-  constructor(private router : Router) { }
-
+  constructor(private router : Router, private menu : MenuController) {
+   }
+  
   ngOnInit() {
     setTimeout(() => {
       this.router.navigate(['checklist'])
       }, 3000);
+  }
+
+  ionViewWillEnter(){
+    this.menu.enable(false);
+  }
+
+  ionViewDidLeave() {
+    // enable the root left menu when leaving the tutorial page
+    this.menu.enable(true);
   }
 }
