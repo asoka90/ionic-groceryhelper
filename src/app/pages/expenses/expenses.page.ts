9c10665e5c9c685b9bdcc7f334673b8afcd57c5d
Keith <limbokeith71@gmail.com>
2021-07-15 13:43:29 +0800

    update on budget and expenses

--- src/app/pages/expenses/expenses.page.ts
+++ src/app/pages/expenses/expenses.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-expenses',
@@ -6,10 +7,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expenses.page.scss'],
 })
 export class ExpensesPage implements OnInit {
+  items: any [] = [];
 
-  constructor() { }
+  constructor(public alert : AlertController) { }
 
   ngOnInit() {
   }
 
+  customTB(index, item){
+    return '${index}-${item.id}';
+  }
+
+  test(){
+    alert("Hey");
+  }
+
+  add(){
+    this.alert.create({
+      header: "Add",
+      inputs: [
+        {
+          name: 'expensess',
+          type: 'text',
+          placeholder: 'Title'
+        }
+      ],
+      buttons: [
+        {
+            text: 'No',
+            handler: () => {
+              console.log('I care about humanity');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: (alertData) => {
+            this.items.push(alertData.expensess);
+            console.log(this.items);
+          }
+        } 
+      ]
+    }).then(res => {
+      res.present();
+    });
+  }
+
+  delete(i){
+    this.alert.create({
+      header: 'Delete',
+      message: 'Are you sure you want to delete this?',
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {
+            console.log('I care about humanity');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.items.splice(i, 1);
+          }
+        }
+    ]
+    }).then(res => {
+      res.present();
+    });
+  }
 }
