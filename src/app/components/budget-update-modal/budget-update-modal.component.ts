a640cee5cbbe0eddd83396145481105b1bb86eda
Keith <limbokeith71@gmail.com>
2021-07-18 19:45:47 +0800

    added budget-update-modal

---
+++ src/app/components/budget-update-modal/budget-update-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-update-modal',
+  templateUrl: './budget-update-modal.component.html',
+  styleUrls: ['./budget-update-modal.component.scss'],
+})
+export class BudgetUpdateModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
